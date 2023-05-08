662c089eb6a8ed10737fd7607918b715d7f886c3
Viktor Kindrat <kindratvictor07@gmail.com>
2023-05-08 21:49:16 +0300

    create Main component. bug fixes

--- src/Componets/App/App.jsx
+++ src/Componets/App/App.jsx
@@ -1,11 +1,13 @@
 import './Styles/App.css';
 
 import Header from '../Header/Header';
+import Main from "../Main/Main"
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Componets/Main/Main.jsx
@@ -0,0 +1,11 @@
+import "./Styles/Main.css"
+
+function Main () {
+    return (
+        <main className="Main">
+
+        </main>
+    )
+}
+
+export default Main
