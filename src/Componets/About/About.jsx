296307da76239200a6b146764b1c5315a2a5a6bd
Viktor Kindrat <kindratvictor07@gmail.com>
2023-05-10 22:15:22 +0300

    add About section

---
+++ src/Componets/About/About.jsx
@@ -0,0 +1,11 @@
+import "./Style/About.css"
+
+function About () {
+    return (
+        <div className="About">
+            ABOUT ME PAGE (in development)
+        </div>
+    )
+}
+
+export default About
--- src/Componets/Main/Main.jsx
+++ src/Componets/Main/Main.jsx
@@ -1,10 +1,12 @@
 import "./Styles/Main.css"
 import Home from "../Home/Home"
+import About from "../About/About"
 
 function Main () {
     return (
         <main className="Main">
             <Home/>
+            <About/>
         </main>
     )
 }
