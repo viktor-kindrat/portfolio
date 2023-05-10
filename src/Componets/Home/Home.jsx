c5560ebf846759c7eea9cf6f3532ff18629e66cc
Viktor Kindrat <kindratvictor07@gmail.com>
2023-05-10 20:18:44 +0300

    update home page. animations add

--- src/Componets/Home/Home.jsx
+++ src/Componets/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from "react"
 import Typed from 'typed.js';
+import { gsap } from "gsap";
 
 import "./Styles/Home.css"
 import dldSvg from "./Images/dldBtn.svg"
@@ -9,6 +10,21 @@ import MainBodyAnimation from "../MainBodyAnimation/MainBodyAnimation"
 function Home() {
     const typedEl = useRef(null);
     useEffect(() => {
+        const setTl = ()=>{
+            let tl = gsap.timeline()
+            tl.set(".Home__move-content", {
+                xPercent: -100,
+                opacity: 0,
+            })
+            setTimeout(() => {
+                tl.to(".Home__move-content", {
+                    xPercent: 0,
+                    opacity: 1,
+                    duration: 0.5,
+                    stagger: 0.3
+                })
+            }, 2800);
+        }
         const typed = new Typed(typedEl.current, {
             strings: ['Viktor Kindtat', 'Web developer', 'Web designer', 'Coding fan'],
             typeSpeed: 50,
@@ -17,20 +33,22 @@ function Home() {
             loopCount: Infinity,
             showCursor: false,
         });
+        window.addEventListener("load", setTl())
         return () => {
             typed.destroy();
+            window.removeEventListener("load", setTl())
         };
     }, []);
     return (
         <section className="Home">
             <div className="Home__content">
                 <h1 className="Home__headline">
-                    <span className="Home__headline_small">Welcome to the page of</span>
-                    <span className="Home__typed-container">
+                    <span className="Home__headline_small Home__move-content">Welcome to the page of</span>
+                    <span className="Home__typed-container Home__move-content">
                         <span ref={typedEl} className="Home__headline_big">Viktor Kindtat</span>
                     </span>
                 </h1>
-                <a className="Home__btn" href={CV} download="CV"> <img height="24" width="24" src={dldSvg} alt="download" className="Home__btn-icon" /> download cv</a>
+                <a className="Home__btn Home__move-content" href={CV} download="CV"> <img height="24" width="24" src={dldSvg} alt="download" className="Home__btn-icon" /> download cv</a>
             </div>
             <MainBodyAnimation />
         </section>
--- src/Componets/MainBodyAnimation/MainBodyAnimation.jsx
+++ src/Componets/MainBodyAnimation/MainBodyAnimation.jsx
@@ -1,9 +1,28 @@
+import { gsap } from "gsap";
+import { useEffect } from "react";
 import "./Styles/MainBodyAnimation.css"
 
 import hand from "./Images/hand.png"
 import body from "./Images/man.png"
 
 function MainBodyAnimation(){
+    useEffect(()=>{
+        let setTl = () => {
+            setTimeout(() => {
+                let tl = gsap.timeline();
+                tl.fromTo(".MainBodyAnimation", {
+                    right: "-100%"
+                }, {
+                    right: "-5px",
+                    duration: 0.6
+                })
+            }, 2800);
+        }
+        window.addEventListener("load", setTl());
+        return (
+            window.removeEventListener("load", setTl())
+        )
+    }, [])
     return (
         <div className="MainBodyAnimation">
             <img height="500" className="MainBodyAnimation__image MainBodyAnimation__image-body" src={body} alt="body" />
