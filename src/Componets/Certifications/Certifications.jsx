db2b9e75a09d20cfc7fde9516b05c6996187eea2
Viktor Kindrat <kindratvictor07@gmail.com>
2023-05-15 19:12:44 +0300

    fetching data from API

--- src/Componets/Certifications/Certifications.jsx
+++ src/Componets/Certifications/Certifications.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState, useRef } from "react"
 import "./Style/Certifications.css"
 
+
 function Certifications (){
+    let [pending, setPending] = useState(true);
+    let certificates = useRef(null)
+    useEffect(()=>{
+        fetch("https://portfolio-api-5x6x.onrender.com/db/getCertificates")
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            certificates.current = data;
+            setPending(false)
+        })
+    }, [])
     return (
         <section className="Certifications">
             <h2 className="Certifications__headline">Certifications</h2>
+            {
+                (certificates.current) ? certificates.current.map(el =>
+                    <div>{el["_doc"].name} <br /> <img height={300} src={`data:${el["_doc"].image.contentType};base64,${el["_doc"].image.data}`} alt="certificate" /></div>
+                ) : ""
+            }
         </section>
     )
 }
