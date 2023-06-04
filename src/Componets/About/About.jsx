be5ad355f796d20518163558c985686d7ca484fb
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-04 21:14:41 +0300

    full logic

--- src/Componets/About/About.jsx
+++ src/Componets/About/About.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { gsap } from "gsap"
+import { Observer } from "gsap/Observer";
 
 import "./Style/About.css"
 
 import image1 from "./Images/1.webp"
 import image2 from "./Images/2.webp"
+
 let data = [{
     img: image1,
     text: "Modern and enthusiastic - that's how those who know me describe me. I'm a 17-year-old web developer from Ukraine who has a great passion for programming and can't imagine his life without it. I am a fast learner and enjoy finding innovative solutions to complex challenges. I have excellent communication skills and work collaboratively with colleagues and clients to ensure projects are delivered to their satisfaction."
@@ -13,58 +15,65 @@ let data = [{
     text: "Embarking on my programming journey, I was fortunate to receive top-notch training at the GoITeens Academy. Here, I had the opportunity to explore and experiment with a variety of programming languages, ultimately discovering my true passion for JavaScript. With my newfound zeal for coding, I enrolled in the highly regarded Creator IT Academy, where I honed my skills in HTML, CSS, and JavaScript, learning the ins and outs of creating stunning websites. Creator was instrumental in equipping me with the technical and soft skills that have led to my success in the field thus far. Now, I am constantly seeking to enhance my knowledge and capabilities, always pushing myself to learn more and stay ahead of the curve."
 }]
 
+gsap.registerPlugin(Observer);
 function About() {
+    let element = useRef(null);
     let [sliderIndex, setSliderIndex] = useState(0)
     useEffect(() => {
-        let root = document.querySelector("html");
-        let firstPage = document.querySelector(".Home");
-        let thisPage = document.querySelector(".About");
-        let placeHolder = document.querySelector(".AboutPlaceHolder")
-        let placeHolderPre = document.querySelector(".AboutPlaceHolder_pre")
-
-        let screenHeight = root.clientHeight;
-        let disableScrollCoordinate = Math.floor(thisPage.offsetTop + parseFloat(getComputedStyle(thisPage).height.slice(0, getComputedStyle(firstPage).height.indexOf("px"))) + screenHeight * (data.length - 1));
-        let scrollHandler = (e) => {
-            let firstPageHeight = Math.floor(parseFloat(getComputedStyle(firstPage).height.slice(0, getComputedStyle(firstPage).height.indexOf("px"))))
-            let scrollTop = Math.floor(root.scrollTop);
-
-            // console.log(`scroll top is: ${scrollTop}\ndisableScrollCordinate is: ${disableScrollCoordinate - screenHeight - 50}\nindex is: ${sliderIndex}`)
-
-            if (scrollTop >= firstPageHeight && scrollTop <= disableScrollCoordinate) {
-                thisPage.setAttribute("style", "position: fixed; top:0; left: 0; transition: 0.3s;");
-                placeHolder.style.display = "block"
-                placeHolderPre.style.display = "none"
-                if (scrollTop > firstPageHeight + 150 && sliderIndex === 0) {
-                    let tl = gsap.timeline();
-                    tl.to(".About__image", { opacity: 0, xPercent: -200, duration: 0.3, ease: "circ.inOut" })
-                    tl.to(".About__text", { opacity: 0, xPercent: 200, duration: 0.3, ease: "circ.inOut" })
-                    setSliderIndex(1)
-                } else if (scrollTop < firstPageHeight + 150 && sliderIndex === 1) {
-                    let tl = gsap.timeline();
-                    tl.to(".About__image", { opacity: 0, xPercent: -200, duration: 0.3, ease: "circ.inOut" })
-                    tl.to(".About__text", { opacity: 0, xPercent: 200, duration: 0.3, ease: "circ.inOut" })
-                    setSliderIndex(0)
-                }
-            } else if (scrollTop > disableScrollCoordinate || scrollTop <= firstPageHeight) {
-                thisPage.setAttribute("style", "position: relative; top:0; left: 0; transition: 0.3s;")
-                placeHolder.style.display = "none"
-                if (scrollTop <= firstPageHeight) {
-                    placeHolderPre.style.display = "none"
-                } else {
-                    placeHolderPre.style.display = "block"
-                }
+        const previous = () => {
+            if (sliderIndex > 0) {
+                setSliderIndex(sliderIndex - 1);
+                element.current.scrollIntoView({ behavior: "smooth" })
+            } else {
+                document.querySelector(".Home").scrollIntoView({ behavior: "smooth" })
             }
         }
-        window.addEventListener("scroll", scrollHandler)
+        const next = () => {
+            if (sliderIndex < data.length - 1) {
+                setSliderIndex(sliderIndex + 1);
+                element.current.scrollIntoView({ behavior: "smooth" })
+            } else {
+                document.querySelector(".Certifications").scrollIntoView({ behavior: "smooth" })
+            }
+        }
+
+        let viewed = false;
+
+        let scrollObserver = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    if (!viewed) {
+                        element.current.scrollIntoView({ behavior: "smooth" })
+                        viewed = true
+                    }
+                    Observer.create({
+                        target: document.querySelector(".About"),
+                        type: "wheel,touch,pointer",
+                        onUp: () => previous(),
+                        onDown: () => next(),
+                        tolerance: 175,
+                        dragMinimum: 175,
+                        wheelSpeed: 0.5,
+                        preventDefault: true,
+                        capture: true,
+                    });
+                } else {
+                    Observer.getAll(o => o.kill());
+                    viewed = false
+                }
+            });
+        });
+        if (element.current) {
+            scrollObserver.observe(element.current)
+        }
         return () => {
-            window.removeEventListener("scroll", scrollHandler)
+            Observer.getAll(o => o.kill());
+            // eslint-disable-next-line
+            scrollObserver.unobserve(element.current)
         }
     }, [sliderIndex])
     useEffect(() => {
         let tl = gsap.timeline();
-        // tl.set(".About__content-inner", { opacity: 0, y: 25 })
-        // tl.to(".About__content-inner", { opacity: 1, y: 0, duration: 0.5, ease: "power2.in" })
-
         tl.set(".About__image", { opacity: 0, xPercent: -200 })
         tl.set(".About__text", { opacity: 0, xPercent: 200 })
         tl.then(() => {
@@ -73,35 +82,21 @@ function About() {
         })
     }, [sliderIndex])
     return (
-        <>
-            <div className="AboutPlaceHolder_pre" style={{ display: "none", "height": ((data.length - 1) * 100) + "vh", "width": 100 + "%" }}></div>
-            <section className="About" id="about">
-                <div className="About__content">
-                    <div className="About__content-wrap">
-                        <h2 className="About__headline">My story</h2>
-                        <div className="About__content-inner">
-                            <img src={data[sliderIndex].img} alt="me" className="About__image" />
-                            <p className="About__text">
-                                {data[sliderIndex].text}
-                            </p>
-                        </div>
-                        <div className="About__progress-bar-container">
-                            {/* this is {((sliderIndex + 1) / data.length ) * 100}% here */}
-                            <div className="About__progress-bar">
-                                <div className="About__progress-bar-value-text">{((sliderIndex + 1) / data.length) * 100}%</div>
-                                <div className="About__progress-bar-value">
-                                    <div style={{ width: `${((sliderIndex + 1) / data.length) * 100}%` }} className="About__progress-bar-value-block"></div>
-                                </div>
-                            </div>
-                            <div className="About__progress-hint">
-                                scroll down
-                            </div>
-                        </div>
+        <section ref={element} className="About" id="about">
+            <div className="About__content">
+                <div className="About__content-wrap">
+                    <h2 className="About__headline">My story</h2>
+                    <div className="About__content-inner">
+                        <img src={data[sliderIndex].img} alt="me" className="About__image" />
+                        <p className="About__text">{data[sliderIndex].text}</p>
+                    </div>
+                    <div className="About__progress-bar-container">
+                        <div className="About__progress-bar"><div className="About__progress-bar-value-text">{((sliderIndex + 1) / data.length) * 100}%</div><div className="About__progress-bar-value"><div style={{ width: `${((sliderIndex + 1) / data.length) * 100}%` }} className="About__progress-bar-value-block"></div></div></div>
+                        <div className="About__progress-hint">scroll down</div>
                     </div>
                 </div>
-            </section>
-            <div className="AboutPlaceHolder" style={{ display: "none", "height": data.length * 100 + "vh", "width": 100 + "%" }}></div>
-        </>
+            </div>
+        </section>
     )
 }
 
