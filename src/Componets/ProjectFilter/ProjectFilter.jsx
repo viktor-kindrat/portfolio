17365c4b0a7f95c83b01e0e1a04d56837e6481de
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-11 20:21:49 +0300

    Filter style

--- src/Componets/ProjectFilter/ProjectFilter.jsx
+++ src/Componets/ProjectFilter/ProjectFilter.jsx
@@ -1,15 +1,16 @@
 import "./Styles/ProjectFilter.css"
+import downArrow from "./Images/arrowdown.svg"
 import filterIcon from "./Images/filterIcon.svg"
 
 import { useState, useCallback, useEffect, useRef } from "react"
 import { gsap } from "gsap"
 
-function ProjectFilter ({data, setData}) {
+function ProjectFilter({ data, setData }) {
     let [filtersVisibility, setFiltersVisibility] = useState(false);
     let [res, setRes] = useState([...data]);
     const dataCopy = useRef([...data])
 
-    let togglerOfFilter = useCallback((e)=> {
+    let togglerOfFilter = useCallback((e) => {
         console.log(!filtersVisibility)
         setFiltersVisibility(!filtersVisibility)
     }, [filtersVisibility]);
@@ -20,27 +21,42 @@ function ProjectFilter ({data, setData}) {
         type: "all"
     })
 
-    let filter = ()=>{
-        let nameFilter = [...dataCopy.current].filter(item=>item._doc.name.toLowerCase().includes(filterCore.current.name.toLowerCase()))
-        let nameAndTechFilter = nameFilter.filter(item=>{
-            return item._doc.features.map(item1=>item1.toLowerCase()).filter(item2=>item2.includes(filterCore.current.tech.toLowerCase())).length > 0 || item._doc.name.toLowerCase().includes(filterCore.current.tech) || item._doc.description.toLowerCase().includes(filterCore.current.tech)
+    let filter = () => {
+        let nameFilter = [...dataCopy.current].filter(item => item._doc.name.toLowerCase().includes(filterCore.current.name.toLowerCase()))
+        let nameAndTechFilter = nameFilter.filter(item => {
+            return item._doc.features.map(item1 => item1.toLowerCase()).filter(item2 => item2.includes(filterCore.current.tech.toLowerCase())).length > 0 || item._doc.name.toLowerCase().includes(filterCore.current.tech) || item._doc.description.toLowerCase().includes(filterCore.current.tech)
         })
-        console.log("Name and thech filtered arr: ",nameAndTechFilter)
+        console.log("Name and thech filtered arr: ", nameAndTechFilter)
         let filtered;
-        (filterCore.current.type !== "all") ? filtered = nameAndTechFilter.filter(item=>item._doc.type === filterCore.current.type) : filtered = nameAndTechFilter;
+        (filterCore.current.type !== "all") ? filtered = nameAndTechFilter.filter(item => item._doc.type === filterCore.current.type) : filtered = nameAndTechFilter;
         console.log(filtered)
         return filtered
     }
-    let changeHandler = useCallback((e)=>{
+    let changeHandler = useCallback((e) => {
         let target = e.target;
         let role = target.dataset.role;
         filterCore.current[role] = target.value;
         setRes(filter())
     }, [filterCore])
 
-    useEffect(()=>{
+    const formInputFocus = useCallback((e) => {
+        const label = e.target.parentElement.children[0];
+        label.style.top = "-5px";
+        label.style.left = "10px";
+        label.style.fontSize = "11px";
+        label.style.fontWeight = "700";
+    }, [])
+
+    const formInputBlur = useCallback((e) => {
+        const label = e.target.parentElement.children[0];
+        if (e.target.value.length < 1) {
+            label.removeAttribute("style")
+        }
+    }, [])
+
+    useEffect(() => {
         let tl = gsap.timeline();
-        if(filtersVisibility) {
+        if (filtersVisibility) {
             tl.to(".ProjectFilter__animate", {
                 opacity: 1,
                 y: 0,
@@ -59,13 +75,13 @@ function ProjectFilter ({data, setData}) {
         }
     }, [filtersVisibility])
 
-    useEffect(()=>{
+    useEffect(() => {
         setData(res)
     }, [res, setData])
     return (
         <div className="ProjectFilter">
             <button onClick={togglerOfFilter} type="button" className="ProjectFilter__thumbler"><img height={15} src={filterIcon} alt="filter" />Filter</button>
-            <select onChange={changeHandler} name="projectType" data-role="type" id="typeFilter" className="ProjectFilter__animate ProjectFilter__select" defaultValue="all">
+            <select onChange={changeHandler} style={{ backgroundImage: `url("${downArrow}")` }} name="projectType" data-role="type" id="typeFilter" className="ProjectFilter__animate ProjectFilter__select" defaultValue="all">
                 <option value="all" className="ProjectFilter__option">All types</option>
                 <option value="pet" className="ProjectFilter__option">Pet project</option>
                 <option value="commercial" className="ProjectFilter__option">Commercial</option>
@@ -73,12 +89,13 @@ function ProjectFilter ({data, setData}) {
             </select>
             <div className="ProjectFilter__animate ProjectFilter__input-group">
                 <label htmlFor="filterByTechnology" className="ProjectFilter__input-placeholder">Search technology</label>
-                <input onInput={changeHandler} type="text" id="filterByTechnology" data-role="tech" className="ProjectFilter__input" />
+                <input onFocus={formInputFocus} onBlur={formInputBlur} onInput={changeHandler} type="text" id="filterByTechnology" data-role="tech" className="ProjectFilter__input" />
             </div>
             <div className="ProjectFilter__animate ProjectFilter__input-group">
                 <label htmlFor="filterByName" className="ProjectFilter__input-placeholder">Search by name</label>
-                <input onInput={changeHandler} type="text" id="filterByName" data-role="name" className="ProjectFilter__input" />
+                <input onFocus={formInputFocus} onBlur={formInputBlur} onInput={changeHandler} type="text" id="filterByName" data-role="name" className="ProjectFilter__input" />
             </div>
+            {filterCore.current.name === "" && filterCore.current.type === "all" && filterCore.current.tech === "" ? "" : <button className="ProjectsFilter__clear-btn">Reset</button>}
         </div>
     )
 }
